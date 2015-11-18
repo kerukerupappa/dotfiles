bdb89379126b4a3fa604d6157058d7be110f8574
鎌田 敏弘 <kerukerupappa@gmail.com>
2015-11-18 14:41:22 +0900

    gitの設定を追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,7 +4,13 @@ ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin
 
 git config --global core.excludesfile ~/.gitignore_global
 git config --global diff.excludesfile ~/dotfiles/.git_diff_wrapper
 git config --global diff.tool vimdiff
+git config --global color.ui auto
+git config --global pager.log 'diff-highlight | less'
+git config --global pager.show 'diff-highlight | less'
+git config --global pager.diff 'diff-highlight | less'
+
